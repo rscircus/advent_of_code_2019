7db5b96827650c5955ab307e44876a8b6aa50846
Roland Siegbert <roland@siegbert.info>
2019-12-02 15:01:59 +0100

    Solution of 1st advent (naive)

--- 1.py
+++ 1.py
@@ -1,5 +1,26 @@
+import math
+
 # Load 1.txt
+arr = list()
+file = open("./1.txt","r")
+
+for line in file:
+    if line:
+        arr.append(float(line))
+
+# Debug:
+# for idx, line in enumerate(arr):
+#     print(str(idx) + ": " + str(line))
+
 # For each line in 1
+
+sum = 0
+for mass in arr:
+    mass = int(math.floor(mass / 3.0) - 2)
+    sum += mass
+
+print(sum)
+
 # # Convert to float
 # # Divide by 3
 # # Floor
