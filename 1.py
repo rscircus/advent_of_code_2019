5e95347fd82f8c6adde5eea9d85c0305c7c63a25
Roland Siegbert <roland@siegbert.info>
2019-12-02 15:04:34 +0100

    Clean up first solution

--- 1.py
+++ 1.py
@@ -1,29 +1,20 @@
 import math
 
-# Load 1.txt
-arr = list()
-file = open("./1.txt","r")
+def get_fuel_requirements():
+    arr = list()
 
-for line in file:
-    if line:
-        arr.append(float(line))
+    # read in file
+    file = open("./1.txt","r")
+    for line in file:
+        if line:
+            arr.append(float(line))
 
-# Debug:
-# for idx, line in enumerate(arr):
-#     print(str(idx) + ": " + str(line))
+    # calculate fuel requirements
+    sum = 0
+    for mass in arr:
+        mass = int(math.floor(mass / 3.0) - 2)
+        sum += mass
 
-# For each line in 1
+    print(sum)
 
-sum = 0
-for mass in arr:
-    mass = int(math.floor(mass / 3.0) - 2)
-    sum += mass
-
-print(sum)
-
-# # Convert to float
-# # Divide by 3
-# # Floor
-# # Subtract 2
-# # Sum to previous sum
-# Return sum
+get_fuel_requirements()
